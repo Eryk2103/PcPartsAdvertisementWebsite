e60763efc654f8bce13f39cc26b16f560f845381
Eryk Ciesielski <eryk0399@gmail.com>
2023-03-02 16:26:13 +0100

    add gpu offer filters

--- src/Controller/GpuOfferController.php
+++ src/Controller/GpuOfferController.php
@@ -7,7 +7,6 @@ use App\Entity\GpuOffer;
 use App\Form\GpuOfferType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,43 +23,83 @@ class GpuOfferController extends AbstractController
     #[Route('/karty-graficzne', name: 'app_gpu_offer')]
     public function index(Request $request): Response
     {
+        //params
         $pageParam = $request->query->get('page');
         $orderParam = $request->query->get('orderby');
-
+        $priceFromParam = $request->query->get('price_from');
+        $priceToParam = $request->query->get('price_to');
+        $manufacturerParam = $request->query->all('manufacturer');
         $orderArr = explode('_', $orderParam);
+
         if($orderParam == null)
         {
             $orderArr[0] = 'createdAt';
             $orderArr[1] = 'desc';
         }
         
-
-        $page = $pageParam == null ? "1" : $pageParam;
-        $itemsPerPage = 5;
-
+        //query builder
         $repo = $this->em->getRepository(GpuOffer::class);
-
-        $count = $repo->createQueryBuilder('g')
+        $qb = $repo->createQueryBuilder('g')
             ->join('g.offer', 'o')
-            ->orderBy('o.' . $orderArr[0], $orderArr[1])
+            ->orderBy('o.' . $orderArr[0], $orderArr[1]);
+        if($manufacturerParam)
+        {
+           
+            $qb -> andWhere('g.manufacturer in (:m)')
+                ->setParameter('m', $manufacturerParam);
+            
+        }
+        if($priceFromParam)
+        {
+            $qb -> andWhere('o.price >= :priceFrom')
+            ->setParameter('priceFrom', $priceFromParam);
+        }
+        if($priceToParam)
+        {
+            $qb -> andWhere('o.price <= :priceTo')
+            ->setParameter('priceTo', $priceToParam);
+        }
+
+        //query builder for getting number of rows
+        $qbCount = $repo->createQueryBuilder('g')
+            ->join('g.offer', 'o')
+            ->orderBy('o.' . $orderArr[0], $orderArr[1]);
+
+            if($manufacturerParam)
+            {
+               
+                $qbCount -> andWhere('g.manufacturer in (:m)')
+                    ->setParameter('m', $manufacturerParam);
+                
+            }
+            if($priceFromParam)
+            {
+                $qbCount -> andWhere('o.price >= :priceFrom')
+                ->setParameter('priceFrom', $priceFromParam);
+            }
+            if($priceToParam)
+            {
+                $qbCount -> andWhere('o.price <= :priceTo')
+                ->setParameter('priceTo', $priceToParam);
+            }    
+
+        $count = $qbCount
             ->select('count(g.id)')
             ->getQuery()
             ->getSingleScalarResult();
 
+        //pagination
+        $page = $pageParam == null ? "1" : $pageParam;
+        $itemsPerPage = 5;
         $lastPage = ceil($count / $itemsPerPage);
         $page = $page < 1 ? 1 : $page;
         $page = $page > $lastPage ? $lastPage : $page;
 
-        $offers = $repo->createQueryBuilder('g')
-            ->join('g.offer', 'o')
-            ->orderBy('o.' . $orderArr[0], $orderArr[1])
+        $offers = $qb
             ->setFirstResult($itemsPerPage * ($page-1))
             ->setMaxResults($itemsPerPage)
             ->getQuery()
             ->getResult();
-
-
-
 
         return $this->render('gpu_offer/index.html.twig', [
             'offers' => $offers,
